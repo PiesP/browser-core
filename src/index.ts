af60f72109308e09d8689541c0d2fdde3a295e60
PiesP <15217557+PiesP@users.noreply.github.com>
2026-07-16 21:26:35 +0900

    chore: initialize @piesp/browser-core package skeleton

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// @piesp/browser-core — main entry
+export * from './async/index.js';
+export * from './error/index.js';
+export * from './locale/index.js';
+export * from './logging/index.js';
+export * from './util/index.js';
