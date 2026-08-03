f38568963e19fc172e53917c177c4d3d33ff97b6
PiesP <15217557+PiesP@users.noreply.github.com>
2026-08-03 09:41:47 +0900

    refactor(core): remove redundant root exports

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,3 @@ export * from './events/index.js';
 export * from './locale/index.js';
 export * from './logging/index.js';
 export * from './util/index.js';
-
-// Re-export util sub-modules for direct imports
-export { cx } from './util/cx.js';
-export { escapeRegExp } from './util/escape-reg-exp.js';
-export { isRecord, isHTMLElement, createEventListener } from './util/guards.js';
-export { computePercentage } from './util/math.js';
-export { resolveNestedPath } from './util/object.js';
-export { generateUniqueId, createPrefixedId } from './util/unique-id.js';
-export { clampSoft, clampMinmax } from './util/clamp-extended.js';
-export { LruMap } from './util/lru-map.js';
-export { ByteLimitedCache } from './util/byte-limited-cache.js';
-export { ResizableByteLimitedCache } from './util/resizable-byte-limited-cache.js';
-export { PriorityBucketQueue } from './util/priority-queue.js';
-export { schedulerYield, schedulerPostTask, yieldIfOverBudget, yieldIfOverBudgetAsync } from './util/scheduler.js';
-export { parseAnyColor, toRgba, computeReadableTextColor } from './util/color.js';
